07c54e96a5c04aea2da884d35bd31d32b532e62a
ramanan99 <venkatramanan@gmail.com>
2016-02-19 18:30:41 +0530

    Please check https://nmskbase.wiki.zoho.com/TLS12.hlp to know more in this regard

---
+++ EchoServer.java
@@ -0,0 +1,66 @@
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.KeyManagerFactory;
+import java.security.KeyStore;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import  java.io.FileInputStream;
+
+public
+class EchoServer {
+    public
+            static
+    void
+            main(String[] arstring) {
+        try {
+		KeyStore             objKeyStore         = KeyStore.getInstance("JKS");
+		KeyManagerFactory    objKeyMngrFactory   = KeyManagerFactory.getInstance("SunX509");
+		SSLContext           objSSLContext       = SSLContext.getInstance("TLSv1");
+
+		objKeyStore.load(new FileInputStream(System.getProperty("javax.net.ssl.keyStore")),System.getProperty("javax.net.ssl.keyStorePassword").toCharArray());
+		objKeyMngrFactory.init(objKeyStore,System.getProperty("javax.net.ssl.keyStorePassword").toCharArray());
+		objSSLContext.init(objKeyMngrFactory.getKeyManagers(),null,null);
+		SSLServerSocketFactory m_sslSockFactory  = objSSLContext.getServerSocketFactory();
+		SSLServerSocket sslserversocket = (SSLServerSocket)m_sslSockFactory.createServerSocket(9999);
+		//String[] enabledCipherSuites = {"SSL_RSA_WITH_RC4_128_MD5", "SSL_RSA_WITH_RC4_128_SHA"};
+		//String[] enabledCipherSuites = {"TLS_RSA_WITH_AES_128_CBC_SHA","TLS_RSA_WITH_AES_256_CBC_SHA"};
+		//sslserversocket.setEnabledCipherSuites(enabledCipherSuites);
+		String protocol = arstring[0];
+		String[] protocols={protocol};
+		sslserversocket.setEnabledProtocols(protocols);
+
+		String[] b = sslserversocket.getEnabledCipherSuites();             
+		if (b != null)
+		{
+
+			StringBuffer result = new StringBuffer();
+			if (b.length > 0) {
+				result.append(b[0]);
+				for (int i=1; i<b.length; i++) {
+					result.append("\n"); // No I18N
+					result.append(b[i]);
+				}
+			}
+			System.out.println("enabled CipherSuites "+result.toString()); // No I18N
+		}    
+		else
+			System.out.println("enabled CipherSuites is null "); // No I18N
+		SSLSocket sslsocket = (SSLSocket) sslserversocket.accept();
+
+            InputStream inputstream = sslsocket.getInputStream();
+            InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
+            BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+
+            String string = null;
+            while ((string = bufferedreader.readLine()) != null) {
+                System.out.println(string);
+                System.out.flush();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
