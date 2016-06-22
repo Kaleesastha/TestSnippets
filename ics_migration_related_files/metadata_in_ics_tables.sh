3d3ea0a780fc88c2a1b7e407c916f21c1228ffe4
ramanan99 <venkatramanan@gmail.com>
2016-06-22 12:24:44 +0530

    script to get metadata of all ics tables
    script to get metadata of all ics tables

---
+++ ics_migration_related_files/metadata_in_ics_tables.sh
@@ -0,0 +1,182 @@
+echo ============================PhysicalEntity====================
+mysql -u root -e 'desc WebNmsDB.PhysicalEntity'
+echo ============================PhysicalContainer====================
+mysql -u root -e 'desc WebNmsDB.PhysicalContainer'
+echo ============================PhysicalElement====================
+mysql -u root -e 'desc WebNmsDB.PhysicalElement'
+echo ============================PhysicalSubUnit====================
+mysql -u root -e 'desc WebNmsDB.PhysicalSubUnit'
+echo ============================PhysicalUnit====================
+mysql -u root -e 'desc WebNmsDB.PhysicalUnit'
+echo ============================LogicalContainer====================
+mysql -u root -e 'desc WebNmsDB.LogicalContainer'
+echo ============================LogicalElement====================
+mysql -u root -e 'desc WebNmsDB.LogicalElement'
+echo ============================LogicalUnit====================
+mysql -u root -e 'desc WebNmsDB.LogicalUnit'
+echo ============================ProtectionGroup====================
+mysql -u root -e 'desc WebNmsDB.ProtectionGroup'
+echo ============================SBNE====================
+mysql -u root -e 'desc WebNmsDB.SBNE'
+echo ============================NetworkElementManagement====================
+mysql -u root -e 'desc WebNmsDB.NetworkElementManagement'
+echo ============================DataObject====================
+mysql -u root -e 'desc WebNmsDB.DataObject'
+echo ============================IpConfig====================
+mysql -u root -e 'desc WebNmsDB.IpConfig'
+echo ============================EventControl====================
+mysql -u root -e 'desc WebNmsDB.EventControl'
+echo ============================RelationObject====================
+mysql -u root -e 'desc WebNmsDB.RelationObject'
+echo ============================PhysicalUnitStream====================
+mysql -u root -e 'desc WebNmsDB.PhysicalUnitStream'
+echo ============================LogicalUnitStream====================
+mysql -u root -e 'desc WebNmsDB.LogicalUnitStream'
+echo ============================LogStreamDataConfig====================
+mysql -u root -e 'desc WebNmsDB.LogStreamDataConfig'
+echo ============================LogStream====================
+mysql -u root -e 'desc WebNmsDB.LogStream'
+echo ============================Constituent====================
+mysql -u root -e 'desc WebNmsDB.Constituent'
+echo ============================ConnectivityData====================
+mysql -u root -e 'desc WebNmsDB.ConnectivityData'
+echo ============================Interface====================
+mysql -u root -e 'desc WebNmsDB.Interface'
+echo ============================InterfaceContainer====================
+mysql -u root -e 'desc WebNmsDB.InterfaceContainer'
+echo ============================Endpoint====================
+mysql -u root -e 'desc WebNmsDB.Endpoint'
+echo ============================BlackBoxLogStream====================
+mysql -u root -e 'desc WebNmsDB.BlackBoxLogStream'
+echo ============================CoreFileMgmtLogStream====================
+mysql -u root -e 'desc WebNmsDB.CoreFileMgmtLogStream'
+echo ============================HapNeControl====================
+mysql -u root -e 'desc WebNmsDB.HapNeControl'
+echo ============================CnpEthernetPortGroup====================
+mysql -u root -e 'desc WebNmsDB.CnpEthernetPortGroup'
+echo ============================CnpFileSystemInfo====================
+mysql -u root -e 'desc WebNmsDB.CnpFileSystemInfo'
+echo ============================CnpRaidPortGroup====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidPortGroup'
+echo ============================CnpServerInfo====================
+mysql -u root -e 'desc WebNmsDB.CnpServerInfo'
+echo ============================CnpEthernetPort====================
+mysql -u root -e 'desc WebNmsDB.CnpEthernetPort'
+echo ============================CnpRaidPort====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidPort'
+echo ============================CnpCageInfo====================
+mysql -u root -e 'desc WebNmsDB.CnpCageInfo'
+echo ============================CnpACL====================
+mysql -u root -e 'desc WebNmsDB.CnpACL'
+echo ============================CnpFruInfo====================
+mysql -u root -e 'desc WebNmsDB.CnpFruInfo'
+echo ============================CnpSystemInfo====================
+mysql -u root -e 'desc WebNmsDB.CnpSystemInfo'
+echo ============================CnpLocationInfo====================
+mysql -u root -e 'desc WebNmsDB.CnpLocationInfo'
+echo ============================CnpMonDevInfo====================
+mysql -u root -e 'desc WebNmsDB.CnpMonDevInfo'
+echo ============================CnpCage====================
+mysql -u root -e 'desc WebNmsDB.CnpCage'
+echo ============================CnpClusterManager====================
+mysql -u root -e 'desc WebNmsDB.CnpClusterManager'
+echo ============================CnpClusterManagerPG====================
+mysql -u root -e 'desc WebNmsDB.CnpClusterManagerPG'
+echo ============================CnpEmsServer====================
+mysql -u root -e 'desc WebNmsDB.CnpEmsServer'
+echo ============================CnpEmsServerPG====================
+mysql -u root -e 'desc WebNmsDB.CnpEmsServerPG'
+echo ============================CnpExtSwitch====================
+mysql -u root -e 'desc WebNmsDB.CnpExtSwitch'
+echo ============================CnpFan====================
+mysql -u root -e 'desc WebNmsDB.CnpFan'
+echo ============================CnpPEM====================
+mysql -u root -e 'desc WebNmsDB.CnpPEM'
+echo ============================CnpRaid====================
+mysql -u root -e 'desc WebNmsDB.CnpRaid'
+echo ============================CnpRaidController====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidController'
+echo ============================CnpRaidFan====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidFan'
+echo ============================CnpRaidLogicalDrive====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidLogicalDrive'
+echo ============================CnpRaidLogicalUnit====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidLogicalUnit'
+echo ============================CnpRaidPEM====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidPEM'
+echo ============================CnpRaidPhysicalDrive====================
+mysql -u root -e 'desc WebNmsDB.CnpRaidPhysicalDrive'
+echo ============================CnpShmm====================
+mysql -u root -e 'desc WebNmsDB.CnpShmm'
+echo ============================CnpSwitch====================
+mysql -u root -e 'desc WebNmsDB.CnpSwitch'
+echo ============================CnpTermServer====================
+mysql -u root -e 'desc WebNmsDB.CnpTermServer'
+echo ============================CnpSwitchUnit====================
+mysql -u root -e 'desc WebNmsDB.CnpSwitchUnit'
+echo ============================CnpRTM====================
+mysql -u root -e 'desc WebNmsDB.CnpRTM'
+echo ============================BlackBoxDataConfig====================
+mysql -u root -e 'desc WebNmsDB.BlackBoxDataConfig'
+echo ============================CnpAirFilter====================
+mysql -u root -e 'desc WebNmsDB.CnpAirFilter'
+echo ============================CnpSystemAlarmPanel====================
+mysql -u root -e 'desc WebNmsDB.CnpSystemAlarmPanel'
+echo ============================CnpShelfEEPROM====================
+mysql -u root -e 'desc WebNmsDB.CnpShelfEEPROM'
+echo ============================CnpShmmBMC====================
+mysql -u root -e 'desc WebNmsDB.CnpShmmBMC'
+echo ============================CnpEmsSensor====================
+mysql -u root -e 'desc WebNmsDB.CnpEmsSensor'
+echo ============================CnpExternalServer====================
+mysql -u root -e 'desc WebNmsDB.CnpExternalServer'
+echo ============================CnpPSBlackBoxLS====================
+mysql -u root -e 'desc WebNmsDB.CnpPSBlackBoxLS'
+echo ============================CnpPSCoreFileMgmtLS====================
+mysql -u root -e 'desc WebNmsDB.CnpPSCoreFileMgmtLS'
+echo ============================CnpPayloadServer====================
+mysql -u root -e 'desc WebNmsDB.CnpPayloadServer'
+echo ============================CnpPayloadServerPG====================
+mysql -u root -e 'desc WebNmsDB.CnpPayloadServerPG'
+echo ============================CnpPSBlackBoxDC====================
+mysql -u root -e 'desc WebNmsDB.CnpPSBlackBoxDC'
+echo ============================CNEOMIManagementEvent====================
+mysql -u root -e 'desc WebNmsDB.CNEOMIManagementEvent'
+echo ============================EMHEvent====================
+mysql -u root -e 'desc WebNmsDB.EMHEvent'
+echo ============================EMHInfoEvent====================
+mysql -u root -e 'desc WebNmsDB.EMHInfoEvent'
+echo ============================ObjectCreationDeletionEvent====================
+mysql -u root -e 'desc WebNmsDB.ObjectCreationDeletionEvent'
+echo ============================StateChangeEvent====================
+mysql -u root -e 'desc WebNmsDB.StateChangeEvent'
+echo ============================UserDefinedEvent====================
+mysql -u root -e 'desc WebNmsDB.UserDefinedEvent'
+echo ============================AttributeValueChangeEvent====================
+mysql -u root -e 'desc WebNmsDB.AttributeValueChangeEvent'
+echo ============================AvailabilityEvent====================
+mysql -u root -e 'desc WebNmsDB.AvailabilityEvent'
+echo ============================RelationshipChangeEvent====================
+mysql -u root -e 'desc WebNmsDB.RelationshipChangeEvent'
+echo ============================EMHAlert====================
+mysql -u root -e 'desc WebNmsDB.EMHAlert'
+echo ============================CNEOMIManagementEvent====================
+mysql -u root -e 'desc WebNmsDB.CNEOMIManagementEvent'
+echo ============================EMHEvent====================
+mysql -u root -e 'desc WebNmsDB.EMHEvent'
+echo ============================EMHInfoEvent====================
+mysql -u root -e 'desc WebNmsDB.EMHInfoEvent'
+echo ============================ObjectCreationDeletionEvent====================
+mysql -u root -e 'desc WebNmsDB.ObjectCreationDeletionEvent'
+echo ============================StateChangeEvent====================
+mysql -u root -e 'desc WebNmsDB.StateChangeEvent'
+echo ============================UserDefinedEvent====================
+mysql -u root -e 'desc WebNmsDB.UserDefinedEvent'
+echo ============================AttributeValueChangeEvent====================
+mysql -u root -e 'desc WebNmsDB.AttributeValueChangeEvent'
+echo ============================AvailabilityEvent====================
+mysql -u root -e 'desc WebNmsDB.AvailabilityEvent'
+echo ============================RelationshipChangeEvent====================
+mysql -u root -e 'desc WebNmsDB.RelationshipChangeEvent'
+echo ============================EMHAlert====================
+mysql -u root -e 'desc WebNmsDB.EMHAlert'
